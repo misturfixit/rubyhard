1f309e364531235f4dd100cc9b3160daee1b0aae
misturfixit <japullins420@gmail.com>
2017-06-30 15:47:58 -0400

    this one

--- exc.1.rb
+++ exc.1.rb
@@ -1,8 +1,9 @@
-puts "Hullo Wurld!"
-puts "hullo again."
-puts "I need practice typing."
-puts "This is fUn."
-puts "yayy!! printingggg!!!"
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
-print "Another line..."
+#puts "Hullo Wurld!"
+#puts "hullo again."
+#puts "I need practice typing."
+#puts "This is fUn."
+#puts "yayy!! printingggg!!!"
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+#print "Another line..."
+p "only this line"
