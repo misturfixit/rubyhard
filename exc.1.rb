6e340a2ce3ecefea00d209fc9c8b4480b2e1d251
misturfixit <japullins420@gmail.com>
2017-06-30 15:10:39 -0400

    first commit

---
+++ exc.1.rb
@@ -0,0 +1,7 @@
+puts "Hullo Wurld!"
+puts "hullo again."
+puts "I need practice typing."
+puts "This is fUn."
+puts "yayy!! printingggg!!!"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
