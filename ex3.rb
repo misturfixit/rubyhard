b5e8f6a7689e9bd432b51e9c3b6d98f7687f2066
misturfixit <japullins420@gmail.com>
2017-07-02 13:09:59 -0400

    comments

--- ex3.rb
+++ ex3.rb
@@ -1,30 +1,36 @@
+# I'm countn chikkens!
 puts "I will now count muh chikkens; "
 
-
+# Ladies first y'all
 puts "Hens #{25 +30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
-
+# and children of course,..
 puts "Now I will count the eggs;"
 
-
+# sum more countn,  spaces aren't necessary... apparently.
 puts 3+2+1-5+4%2-1/4+6
 
 puts "Is it true that 3+2 < 5-7??" 
-
+# even tho not needed(here), spaces don't get read as 
+# anything, (here) so can help with orientation and neatness.
 puts 3+2 < 5-7
-
+                   #{when inna string, this#{} will call or 
+                   #perform a func.}
 puts "What is 3+2? #{3+2}"
 puts "What is 5-7? #{5-7}"
 
-
+# this is just cute
 puts "Oh,.. that's why it's false."
 
-
+         # aahh vvv...
 puts "How about sum (ha)more?? "
-
+      # is that ^^^  a math pun?? 
+      # i'm so ashamed 
 
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
+
+# no func. no end req??
 
