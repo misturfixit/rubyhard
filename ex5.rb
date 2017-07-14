6c70a52a309732c0de99af9b865eb81eae719a71
misturfixit <japullins420@gmail.com>
2017-07-14 15:31:34 -0400

    morestuff

---
+++ ex5.rb
@@ -0,0 +1,22 @@
+
+
+
+my_name = 'Joe'		#just settin 
+my_age = 38      	#some var.
+my_height = 72 
+my_weight = 145
+my_eyes = 'brown'					
+my_teeth = 'eggshell?'					
+my_hair = 'ughh'					
+					
+					
+puts "Lets talk about #{my_name}."					
+puts "He's #{my_height} inches tall."					
+puts "He's #{my_weight} pounds heavy."
+puts "lmao..heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair??"
+puts "His teeth are usually #{my_teeth} depending on coffee and cigarettes."
+
+					
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts ""
