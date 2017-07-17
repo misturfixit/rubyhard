f5764a651ba7d5e1404dcab075f8b760d5fc3f54
misturfixit <japullins420@gmail.com>
2017-07-17 09:24:05 -0400

    sumcomments

--- ex5.rb
+++ ex5.rb
@@ -1,22 +1,22 @@
 
 
 
-my_name = 'Joe'		#just settin 
-my_age = 38      	#some var.
-my_height = 72 
-my_weight = 145
-my_eyes = 'brown'					
-my_teeth = 'eggshell?'					
-my_hair = 'ughh'					
+name = 'Joe'		# just setting 
+age = 38      	# some var.
+height = 72 
+weight = 145
+eyes = 'brown'		# and some more			
+teeth = 'eggshell?'					
+hair = 'ughh'					
 					
 					
-puts "Lets talk about #{my_name}."					
-puts "He's #{my_height} inches tall."					
-puts "He's #{my_weight} pounds heavy."
-puts "lmao..heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair??"
-puts "His teeth are usually #{my_teeth} depending on coffee and cigarettes."
+puts "Lets talk about #{name}."		# now we call 			
+puts "He's #{height} inches tall."	# the var. with #{}				
+puts "He's #{weight} pounds heavy."  # from within the string   
+puts "lmao..heavy."	# def. not heavy
+puts "He's got #{eyes} eyes and #{hair} hair??"
+puts "His teeth are usually #{teeth} depending on coffee and cigarettes."
 
 					
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
 puts ""
